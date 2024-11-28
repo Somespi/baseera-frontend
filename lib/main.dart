7600dbc4da9e5eb05704d79a4657875703ea562e
somespi <abdulalfarsi208@gmail.com>
2024-11-28 13:28:34 +0400

    backup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:flutter/services.dart';
+import 'package:serialport_plus/serialport_plus.dart';
 import 'dart:typed_data';
 import 'package:image/image.dart' as img;
-import 'package:baseerah/preprocess_image.dart';
 import 'package:camerawesome/camerawesome_plugin.dart';
 import 'package:flutter/material.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
+import 'package:usb_serial/usb_serial.dart';
 import 'help_utilities.dart';
 import 'yolo.dart' as yolo;
 
@@ -12,6 +14,7 @@ late List<String> labels;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   labels = await yolo.loadLabels();
+
   runApp(const MyApp());
 }
 
@@ -38,14 +41,22 @@ class MyHomePage extends StatefulWidget {
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
+
 Uint8List encodeAsPng(Uint8List rawData, int width, int height) {
-  final image = img.Image.fromBytes(bytes: rawData.buffer, height: height, width: width);
+  final image =
+      img.Image.fromBytes(bytes: rawData.buffer, height: height, width: width);
   return Uint8List.fromList(img.encodePng(image));
 }
+
 class _MyHomePageState extends State<MyHomePage> {
   bool _isLoading = false;
   late Interpreter _interpreter;
   Uint8List? _img;
+  UsbPort? _port;
+  List _devices = [];
+  Uint8List? _buffer;
+  Uint8List _currentImgBuffer = Uint8List(0);
+  final _serialportFlutterPlugin = SerialportPlus();
 
   @override
   void initState() {
@@ -67,8 +78,9 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   @override
-  void dispose() {
+  void dispose() async {
     _interpreter.close();
+    await _serialportFlutterPlugin.close();
     super.dispose();
   }
 
@@ -81,11 +93,11 @@ class _MyHomePageState extends State<MyHomePage> {
       );
     }
 
-
     return CameraAwesomeBuilder.analysisOnly(
       onImageForAnalysis: (image) async {
-        final detections = await yolo.runObjectDetectionInBackground(yolo.fromJpegToImg(image as JpegImage), _interpreter, labels);
-        printDebug( detections);
+        // final detections = await yolo.runObjectDetectionInBackground(
+        //     yolo.fromJpegToImg(image as JpegImage), _interpreter, labels);
+        // printDebug(detections);
       },
       imageAnalysisConfig: AnalysisConfig(
         androidOptions: const AndroidAnalysisOptions.jpeg(
@@ -95,10 +107,91 @@ class _MyHomePageState extends State<MyHomePage> {
         maxFramesPerSecond: 5,
       ),
       builder: (CameraState state, Preview preview) {
-        return  Scaffold(
-          body: _img != null 
-    ?  Center(child: Image(image: MemoryImage(_img!), height: 640, width: 640,))
-    : const Center(child: CircularProgressIndicator()),
+        return Scaffold(
+          floatingActionButton: FloatingActionButton(
+            onPressed: () async {
+              List<UsbDevice> devices = await UsbSerial.listDevices();
+              printDebug("Found ${devices.length} devices");
+              if (devices.isEmpty) {
+                return;
+              }
+
+              UsbPort? port = await devices[0].create();
+              if (port == null) {
+                printDebug("Failed to create port");
+                return;
+              }
+              _port = port;
+
+              bool openResult = await port.open();
+              if (!openResult) {
+                printDebug("Failed to open port");
+                return;
+              }
+
+              await port.setDTR(true);
+              await port.setRTS(true);
+
+              port.setPortParameters(
+                115200,
+                UsbPort.DATABITS_8,
+                UsbPort.STOPBITS_1,
+                UsbPort.PARITY_NONE,
+              );
+              port.inputStream?.listen((Uint8List event) {
+                _currentImgBuffer =
+                    Uint8List.fromList(_currentImgBuffer + event);
+                const delimiter = '\nDone...';
+                final delimiterIndex =
+                    String.fromCharCodes(_currentImgBuffer).indexOf(delimiter);
+                if (delimiterIndex != -1) {
+                  final imageData =
+                      _currentImgBuffer.sublist(0, delimiterIndex);
+                  _currentImgBuffer = _currentImgBuffer
+                      .sublist(delimiterIndex + delimiter.length);
+
+                  final image = img.decodeJpg(imageData);
+                  if (image != null) {
+                  showDialog(
+                      // ignore: use_build_context_synchronously
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title:  Text("Image data length: ${imageData.length}"),
+                          content: Image.memory(encodeAsPng(
+                              imageData, image.width, image.height)),
+                        );
+                      }); 
+                    setState(() {
+                      _img = encodeAsPng(imageData, image.width, image.height);
+                    });
+                  } else {
+                    showDialog(
+                      // ignore: use_build_context_synchronously
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title:  Text("Image data length: ${imageData.length}"),
+                          content: Text("Error decoding JPEG image."),
+                        );
+                      }); 
+                  }
+                }
+              }, onError: (error) {
+                printDebug("Error reading from port: $error");
+              }, onDone: () {
+                printDebug("Port closed");
+              });
+
+              setState(() {
+                _devices = devices.map((e) => e.deviceName).toList();
+              });
+            },
+            child: const Icon(Icons.usb),
+          ),
+          body: Center(
+            child: _img != null ? Image.memory(_img!) : const Text('No image'),
+          ),
         );
       },
     );
