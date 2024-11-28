0b7642772af7295a78a89e3485d9b28b18382937
somespi <abdulalfarsi208@gmail.com>
2024-11-28 15:22:16 +0400

    add other configs

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
-import 'dart:convert';
-import 'dart:math';
-
+import 'dart:async';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 import 'package:serialport_plus/serialport_plus.dart';
 import 'dart:typed_data';
 import 'package:image/image.dart' as img;
-import 'package:camerawesome/camerawesome_plugin.dart';
 import 'package:flutter/material.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
 import 'package:usb_serial/usb_serial.dart';
 import 'help_utilities.dart';
 import 'yolo.dart' as yolo;
+import 'priority_manager.dart' as priority_manager;
 
 late List<String> labels;
 
@@ -19,6 +19,13 @@ void main() async {
   labels = await yolo.loadLabels();
 
   runApp(const MyApp());
+}
+
+Future<void> _performActionInBackground(Map<String, dynamic> params) async {
+  final weight = params['weight'];
+  final nv21Image = params['nv21Image'];
+
+  await priority_manager.PriorityItem.performStaticAction(weight, nv21Image);
 }
 
 class MyApp extends StatelessWidget {
@@ -54,17 +61,19 @@ Uint8List encodeAsPng(Uint8List rawData, int width, int height) {
 class _MyHomePageState extends State<MyHomePage> {
   bool _isLoading = false;
   late Interpreter _interpreter;
-  Uint8List? _img;
+  // ignore: unused_field
   UsbPort? _port;
-  List _devices = [];
   String? _out;
   Uint8List _currentImgBuffer = Uint8List(0);
   final _serialportFlutterPlugin = SerialportPlus();
+  bool isPersonMoving = false;
+  StreamSubscription? _gyroscopeSubscription;
 
   @override
   void initState() {
     super.initState();
     _initializeModel();
+    _initializeGyroscope();
   }
 
   Future<void> _initializeModel() async {
@@ -80,10 +89,24 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _initializeGyroscope() {
+    const movementThreshold = 0.3;
+    _gyroscopeSubscription =
+        gyroscopeEventStream().listen((GyroscopeEvent event) {
+      final magnitude =
+          (event.x * event.x) + (event.y * event.y) + (event.z * event.z);
+      setState(() {
+        isPersonMoving = magnitude > movementThreshold;
+      });
+    });
+  }
+
   @override
   void dispose() async {
     _interpreter.close();
     await _serialportFlutterPlugin.close();
+    await _port?.close();
+    _gyroscopeSubscription?.cancel();
     super.dispose();
   }
 
@@ -144,11 +167,72 @@ class _MyHomePageState extends State<MyHomePage> {
                 final image = img.decodeJpg(imageData);
 
                 if (image != null) {
-                  final detections = await yolo.runObjectDetectionInBackground(
+                  final detectedObjects = await yolo.runObjectDetectionInBackground(
                       image, _interpreter, labels);
+                  var maxWeight = 'LOW';
+                  priority_manager.PriorityItem? maxObject;
 
+                  var previousFrameObjects = <String, List<double>>{};
+
+                  for (var objectData in detectedObjects) {
+                    String label = objectData['className'];
+                    List<int> currentBox = objectData['bbox'];
+
+                    priority_manager.PriorityItem item =
+                        priority_manager.PriorityItem(
+                      label: label,
+                      isPersonMoving: isPersonMoving,
+                      isObjectMoving: false,
+                      weight: 1.0,
+                      direction: '',
+                    );
+
+                    if (previousFrameObjects
+                        .containsKey(labels[objectData['classIndex']])) {
+                      double previousX = previousFrameObjects[
+                              labels[objectData['classIndex']]]?[0] ??
+                          0.0;
+                      double previousY = previousFrameObjects[
+                              labels[objectData['classIndex']]]?[1] ??
+                          0.0;
+
+                      double dx = currentBox[0] - previousX;
+                      double dy = currentBox[1] - previousY;
+
+                      bool isMoving = (dx.abs() + dy.abs()) > 5;
+                      String direction = '';
+                      if (isMoving) {
+                        if (dx.abs() > dy.abs()) {
+                          direction = dx > 0 ? 'right' : 'left';
+                        } else {
+                          direction = dy > 0 ? 'down' : 'up';
+                        }
+                      }
+                      item.isObjectMoving = isMoving;
+                      item.direction = direction;
+                    }
+
+                    var itemWeight = item.measureWeight();
+                    if (itemWeight == 'HIGH') {
+                      maxWeight = 'HIGH';
+                      maxObject = item;
+                    } else if (itemWeight == 'MEDIUM' && maxWeight != 'HIGH') {
+                      maxWeight = 'MEDIUM';
+                      maxObject = item;
+                    } else if (itemWeight == 'LOW' && maxWeight == 'LOW') {
+                      maxObject ??= item;
+                    }
+                  }
+                  printDebug("Max weight: $maxWeight");
+                  if (maxObject != null) {
+                    final params = {
+                      'weight': maxObject.measureWeight(),
+                      'nv21Image': image,
+                    };
+                    await compute(_performActionInBackground, params);
+                  }
                   setState(() {
-                    _out = '$detections';
+                    _out = '$detectedObjects';
                   });
                 } else {
                   printDebug('Failed to decode JPEG image');
